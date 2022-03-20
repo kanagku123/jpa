a43dfea36e4ea39117a9f313dc003a8850141cdc
github kanagku123 <kanagku123@naver.com>
2022-03-20 23:09:00 +0900

    jpa 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,16 +12,52 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
+            try {
+                /*Member findmember = em.find(Member.class, 1L);
+                findmember.setName("HelloJPA");*/
+/*
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HellA");
+                List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                                .setFirstResult(0)
+                                .setMaxResults(2)
+                                .getResultList();
+                for (Member member : result) {
+                    System.out.println("member.name="+member.getName());
+                }
+*/
+/*
 
-        em.persist(member);
-        tx.commit();
+                //비영속
+                Member member = new Member();
+                member.setId(101L);
+                member.setName("HelloJPA");
 
-        em.close();
+                //영속
+                em.persist(member);
 
+                Member findMember = em.find(Member.class, 101L);
+
+                System.out.println("findMember.id = " +findMember.getId());
+                System.out.println("findMember.name = " +findMember.getName());
+
+*/
+//                Member member1 = new Member(150L,"A");
+//                Member member2 = new Member(160L,"B");
+
+               /* em.persist(member1);
+                em.persist(member2);
+*/
+                // 플러시는 영속성 컨테스트에 있는 걸 db로 동기화 시켜준다. //em.flush();
+
+                   Member member = em.find(Member.class , 150L);
+                member.setName("zzzz");
+
+                tx.commit();
+            }catch (Exception e){
+                tx.rollback();
+            }finally {
+                em.close();
+            }
         emf.close();
     }
 }
